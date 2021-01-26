913692a6b937e15a56fc0db32c9c25582b33c972
Andreas Achilleos <andreasachilleos1994@gmail.com>
2021-01-26 21:27:35 +0000

    created models and routes

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,5 @@
+class ReviewsController < ApplicationController
+  def new
+    @restaurant =
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get "restaurants", to: "restaurant#index"
+
+  # get "restaurants/new", to: "restaurant#new", as: :new_restaurant
+  # post "restaurants", to: "restaurant#create"
+
+  # get "restaurants/:id", to: "restaurant#show", as: :restaurant
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
